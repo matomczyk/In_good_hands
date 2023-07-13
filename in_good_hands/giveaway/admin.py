aea0805b8180467d2a6706a768d9ed087746bacf
matomczyk <tomczykm4@gmail.com>
2023-07-13 22:28:40 +0200

    register model institution

--- in_good_hands/giveaway/admin.py
+++ in_good_hands/giveaway/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Institution
+
 
 # Register your models here.
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    pass
